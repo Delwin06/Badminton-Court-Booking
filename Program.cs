ac05775d07dc0ba2590ebad170787d19dd075faf
Delwin06 <dennisdelwin06@gmail.com>
2026-06-16 22:50:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    DefaultFileNames = new List<string> { "Homepage.html" }  // ✅ your exact filename
+});
+
+app.UseStaticFiles();
+
+app.MapControllers();
+
+app.Run();
